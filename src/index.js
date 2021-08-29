086f95e3e72224781e02ba079eee4104dca28f0b
Jaime Zamora <kratov@protonmail.com>
2021-08-29 14:50:28 -0500

    comment-service create comment

--- src/index.js
+++ src/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const { randomBytes } = require("crypto");
+const {randomBytes} = require("crypto");
 const bodyParser = require("body-parser");
 require('colors');
 
-const app =  express();
+const app = express();
 app.use(bodyParser.json());
 
-const posts = {};
+const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-    res.send(posts);
+    res.send(commentsByPostId[req.params.id] || []);
 })
 
 app.post('/posts/:id/comments', (req, res) => {
-    console.log("\nCreating new post".bgYellow.black);
+    console.log(`\n Creating new comment for ${req.params.id}`.bgYellow.black);
     const id = randomBytes(4).toString('hex');
     console.log(`Id: ${id}`.bgYellow.black);
-    const {title} = req.body;
-    posts[id] = {
-        id, title
-    }
-    console.log(`Created: ${JSON.stringify(posts)}`.bgYellow.black);
-    res.status(201).send(posts[id]);
+    const {content} = req.body;
+    const comments = commentsByPostId[req.params.id] || [];
+    const comment = {id, content};
+    comments.push(comment);
+    commentsByPostId[req.params.id] = comments;
+    console.log(`Created: ${JSON.stringify(comment)}`.bgYellow.black);
+
+    res.status(201).send(comments);
 })
 
 app.listen(4001, () => {
